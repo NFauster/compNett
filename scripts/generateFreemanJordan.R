419c120fe3223fa4fa8e980150776dea7e409a79
NFauster <norman.fauster@outlook.com>
2022-02-22 08:16:03 +0100

    Corrected degree-function: self loop counts as two. Faster implementation of degree computation.

--- scripts/generateFreemanJordan.R
+++ scripts/generateFreemanJordan.R
@@ -5,25 +5,62 @@ library(magrittr)
 
 
 # Functions ----
-degree <- function(edge_list){
-  nodes <- as.vector(edge_list) %>%
-    unique() %>%
-    na.omit()
+degree <- function(edge_list, directed = FALSE){
+  # This function computes the degree for every node in the network given
+  # by the edge list. If the network is directed, the in- and out-degree is
+  # computed. 
+  # Note: A self loop adds two to the degree of a node or one to each in-
+  # and out-degree if the network is directed.
+  #
+  # Arguments
+  # edge_list         a two-column matrix with one row for each edge in the
+  #                   network. The two columns indicate start- and end-node
+  #                   of the edge. If directed = TRUE, the first column is
+  #                   interpreted as starting point.
+  # directed          logical. If TRUE, the network is interpreted as directed,
+  #                   otherwise undirected.
+  #
+  # Value
+  # If directed is FALSE, the result is a named vector with the node degree
+  # as value and the node-name as name.
+  # If directed is TRUE, the result is a named list with two named vectors, one
+  # for the in-degrees, one for the out-degrees.
   
-  
-  degrees <- sapply(nodes, function(node) {
-    apply(edge_list, MARGIN = 1, FUN = function(edge_list) node %in% edge_list) %>%
-      sum()
-  })
-  
-  names(degrees) <- nodes
-  
-  return(degrees)
+  # Directed network
+  if(directed){
+    ## Out degrees
+    from_freq <- as.vector(edge_list[,1]) %>%
+      table(useNA = "no")
+    
+    out_degrees <- as.numeric(from_freq) %>%
+      `names<-`(attr(from_freq, "dimnames")[[1]])
+    
+    ## In degrees
+    to_freq <- as.vector(edge_list[,2]) %>%
+      table(useNA = "no")
+    
+    in_degrees <- as.numeric(to_freq) %>%
+      `names<-`(attr(to_freq, "dimnames")[[1]])
+    
+    ## Return
+    return(list("in_deg" = in_degrees,
+                "out_deg" = out_degrees))
+  }else {
+    # Undirected network
+    node_freq <- as.vector(edge_list) %>%
+      table(useNA = "no")
+    
+    degrees <- as.numeric(node_freq) %>%
+      `names<-`(attr(node_freq, "dimnames")[[1]])
+    
+    ## Return
+    return(degrees)
+  }
 }
 
 
 # Parameters ----
-number_nodes <- 1e2
+number_nodes <- 1e3
 nodes_init_graph <- 1      ## Size of initial (seed) graph
 init_graph <- matrix(ncol = 2)     ## Or specify inital graph (edge list)
 
@@ -49,8 +86,9 @@ n <- edge_list[,-3] %>%
 
 ## Iteration ====
 while(n < number_nodes){
-  tictoc::tic()
+  
   degrees <- degree(edge_list[,-3, drop = FALSE])
+  
   P <- sample(1:n, 
               R_nodes[n], 
               replace = TRUE,
@@ -67,7 +105,7 @@ while(n < number_nodes){
   
   edge_list[n,] <- c(fittest_node, n, n-1)
   
-  tictoc::toc()
+  
 }
 
 
