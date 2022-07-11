138ad117dca282ed8417268d40ea3f13fa09ade0
NFauster <norman.fauster@outlook.com>
2022-07-11 14:04:29 +0200

    temp helper scripts

--- scripts/countingGraphlets_Rcpp.R
+++ scripts/countingGraphlets_Rcpp.R
@@ -1,5 +1,6 @@
 library(Rcpp)
 
+
 sourceCpp(here::here("scripts/countOrtmann.cpp"))
 
 tictoc::tic()
@@ -13,3 +14,7 @@ sourceCpp(here::here("scripts/countOrtmann_1.cpp"))
 # tictoc::tic()
 countOrtmann_1(neighbourhood)
 # tictoc::toc()
+
+
+
+evalCpp("IntegerMatrix(2, 3)")
