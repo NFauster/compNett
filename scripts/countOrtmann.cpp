2ac458b9abbe94b1e7b008b0bd7e575e8e972528
Norman FRM Fauster <norman.fauster@outlook.com>
2022-07-21 16:07:10 +0200

    optimization part I, neighbourhoods stored in array, use iterator for selected neighbourhoods

--- scripts/countOrtmann.cpp
+++ scripts/countOrtmann.cpp
@@ -1,6 +1,8 @@
 #include <RcppArmadillo.h>
+#include <RcppClock.h>
 
 // [[Rcpp::depends(RcppArmadillo)]]
+//[[Rcpp::depends(RcppClock)]]
 
 using namespace Rcpp;
 using namespace arma;
@@ -27,27 +29,27 @@ double spearman(IntegerVector x, IntegerVector y){
 	return result[0];
 }
 
-IntegerVector non_induced_orbits (int crt_node, int n_nodes, int n_edges, 
-							  List neighbourhood, IntegerVector deg, IntegerMatrix edge_list,
+IntegerVector non_induced_orbits (unsigned int crt_node, unsigned int n_nodes, unsigned int n_edges, 
+							  IntegerVector neighbourhood[][3], IntegerVector deg,
 							  IntegerVector k3, IntegerVector c4, IntegerVector k4,
 							  IntegerVector k3_edge[], IntegerVector completing_triangle[]){
 								  
 	IntegerVector nn(20,0);
 	//IntegerVector ni(20,0);
 	
-	IntegerVector crt_N = as<List>(neighbourhood["total_neighbourhood"])[crt_node - 1];
+	IntegerVector crt_N = neighbourhood[crt_node - 1][0];
 	IntegerVector deg_N = deg[crt_N-1];
 	IntegerVector crt_T = completing_triangle[crt_node - 1];
 	
-	int dv_2 = 0;
-	for(int i = 0; i < n_nodes; i++){
+	unsigned int dv_2 = 0;
+	for(unsigned int i = 0; i < n_nodes; i++){
 		dv_2 += chooseC(deg[i], 2);
 	}
 	
 	//add nn10
-	int temp_nn10 = 0;
-	for(int v:crt_N){
-		IntegerVector v_N = as<List>(neighbourhood["total_neighbourhood"])[v - 1];
+	unsigned int temp_nn10 = 0;
+	for(unsigned int v:crt_N){
+		IntegerVector v_N = neighbourhood[v - 1][0];
 		temp_nn10 += sum(as<IntegerVector>(deg[v_N - 1])) - deg[v - 1];
 	}
 	
@@ -64,33 +66,33 @@ IntegerVector non_induced_orbits (int crt_node, int n_nodes, int n_edges,
 	nn[10] = temp_nn10 - deg[crt_node - 1] * (deg[crt_node - 1] - 1) - 2*k3[crt_node - 1];
 	nn[11] = chooseC(deg[crt_node - 1], 3);
 	
-	for(int v: crt_N){
+	for(unsigned int v: crt_N){
 		nn[12] += chooseC(deg[v - 1] - 1, 2);
 	}
 	
 	nn[13] = k3[crt_node - 1]*(deg[crt_node - 1] - 2);
 	
-	for(int i = 0; i < crt_T.length(); i += 2){
+	for(unsigned int i = 0; i < crt_T.length(); i += 2){
 		nn[14] += deg[crt_T[i] - 1] + deg[crt_T[i+1] - 1] - 4;
 	}
 	
 	nn[15] = sum(as<IntegerVector>(k3[crt_N - 1])) - sum(k3_edge[crt_node - 1]);
 	
 	nn[16] = -chooseC(deg[crt_node - 1], 2);
-	for(int i = 0; i < crt_N.length(); i++){
-		IntegerVector v_N = as<List>(neighbourhood["total_neighbourhood"])[crt_N[i] - 1];
-		for(int j = i+1; j < crt_N.length(); j++){
-			IntegerVector w_N = as<List>(neighbourhood["total_neighbourhood"])[crt_N[j] - 1];
+	for(unsigned int i = 0; i < crt_N.length(); i++){
+		IntegerVector v_N = neighbourhood[crt_N[i] - 1][0];
+		for(unsigned int j = i+1; j < crt_N.length(); j++){
+			IntegerVector w_N = neighbourhood[crt_N[j] - 1][0];
 			nn[16] += intersect(v_N, w_N).length();
 		}
 	}
 	
 	nn[17] = -k3[crt_node - 1];
-	for(int i = 0; i < crt_T.length(); i += 2){
+	for(unsigned int i = 0; i < crt_T.length(); i += 2){
 		nn[17] += as<IntegerVector>(k3_edge[crt_T[i] - 1])[to_string(crt_T[i + 1])];
 	}
 	
-	for(int t:k3_edge[crt_node - 1]){
+	for(unsigned int t:k3_edge[crt_node - 1]){
 		nn[18] += chooseC(t,2);
 	}
 	
@@ -134,8 +136,13 @@ IntegerVector compute_induced_orbits(IntegerVector nn){
 
 // [[Rcpp::export]]
 IntegerMatrix countOrtmann(IntegerMatrix edge_list){
-	int n_nodes = max(edge_list);
-	int n_edges = edge_list.nrow();
+	Rcpp::Clock clock;
+	
+	clock.tick("total_count");
+	
+	clock.tick("init");
+	unsigned int n_nodes = max(edge_list);
+	unsigned int n_edges = edge_list.nrow();
 	
 	// initialising variables
 	IntegerVector k3(n_nodes,0);
@@ -143,22 +150,22 @@ IntegerMatrix countOrtmann(IntegerMatrix edge_list){
 	IntegerVector k4(n_nodes,0);
 	
 	IntegerVector k3_edge[n_nodes];
-	for(int i = 0; i < n_nodes; i++){
+	for(unsigned int i = 0; i < n_nodes; i++){
 		k3_edge[i] = IntegerVector::create(Named("-1",0));
 	}
 	
 	IntegerVector completing_triangle[n_nodes];
-	/*for(int i = 0; i < n_nodes; i++){
+	/*for(unsigned int i = 0; i < n_nodes; i++){
 		k3_edge[i] = IntegerVector::create(Named("-1",0));
 	}*/
 	
 	/*IntegerVector k4_edge[n_nodes];
-	for(int i = 0; i < n_nodes; i++){
+	for(unsigned int i = 0; i < n_nodes; i++){
 		k3_edge[i] = IntegerVector::create(Named("-1",0));
 	}
 	
 	IntegerVector c4_edge[n_nodes];
-	for(int i = 0; i < n_nodes; i++){
+	for(unsigned int i = 0; i < n_nodes; i++){
 		k3_edge[i] = IntegerVector::create(Named("-1",0));
 	}*/
 	
@@ -167,47 +174,74 @@ IntegerMatrix countOrtmann(IntegerMatrix edge_list){
 	IntegerVector visited(n_nodes,0);
 	IntegerVector processed(n_nodes,0);
 	
+	clock.tock("init");
+	
+	
 	//call R function
 	Function list_neighbourhood("list_neighbourhood");
 	Function degree("degree");
 	
 	// Compute neighbourhoods
-	List neighbourhood = list_neighbourhood(edge_list, Named("directed") = true);
+	clock.tick("neighbour");
+	List neighbourhood_list = list_neighbourhood(edge_list, Named("directed") = true); //TODO in Rcpp
+	IntegerVector neighbourhood[n_nodes][3];
+	
+	for(unsigned int i = 0; i < n_nodes; i++){
+		neighbourhood[i][0] = as<List>(neighbourhood_list["total_neighbourhood"])[i];
+		neighbourhood[i][1] = as<List>(neighbourhood_list["in_neighbourhood"])[i];
+		neighbourhood[i][2] = as<List>(neighbourhood_list["out_neighbourhood"])[i];
+	}
+	clock.tock("neighbour");
 									
 	// Compute degree vector
+	clock.tick("degree");
 	IntegerVector deg = degree(edge_list, Named("directed") = false);
+	clock.tock("degree");
 	
 	
 	// Algorithm
-	for(int u = 2; u <= n_nodes; u++){
-		IntegerVector u_N_in = as<List>(neighbourhood["in_neighbourhood"])[u-1];
-			
-		for(int v: u_N_in){
+	clock.tick("algorithm");
+	for(unsigned int u = 2; u <= n_nodes; u++){
+		
+		clock.tick("mark_for");
+		for(unsigned int v: neighbourhood[u-1][1]){ //loop over N-(u)
 			mark[v-1] ++;
 		}
+		clock.tock("mark_for");
 		
-		for(int v: u_N_in){
+		clock.tick("count_k3");
+		for(unsigned int v: neighbourhood[u-1][1]){ //loop over N-(u)
 			mark[v-1] --;
 			
-			IntegerVector v_N = as<List>(neighbourhood["total_neighbourhood"])[v-1];
-			IntegerVector v_N_sel = v_N[v_N < u];
+			clock.tick("get_neighbour");
+			// {w e N(v):w < u}
+			IntegerVector v_N_sel(neighbourhood[v-1][0].begin(), 
+								  lower_bound(neighbourhood[v-1][0].begin(), 
+								  neighbourhood[v-1][0].end(), u));
+			clock.tock("get_neighbour");
 			
-			for(int w: v_N_sel){
+			for(unsigned int w: v_N_sel){
 				visited[w-1] ++;
 				processed[w-1] ++;
 			}
 			
+			clock.tick("get_neighbour");
+			// {w e N+(v):w < u}
+			IntegerVector v_N_out_sel(neighbourhood[v-1][2].begin(), 
+									  lower_bound(neighbourhood[v-1][2].begin(),
+									  neighbourhood[v-1][2].end(), u));
+			clock.tock("get_neighbour");
 			
-			IntegerVector v_N_out = as<List>(neighbourhood["out_neighbourhood"])[v-1];
-			IntegerVector v_N_out_sel = v_N_out[v_N_out < u];
-			
-			for(int w: v_N_out_sel){
+			for(unsigned int w: v_N_out_sel){
 				mark[w-1] += 2;
 			}
-			for(int w: v_N_out_sel){
+			
+			clock.tick("w_for");
+			for(unsigned int w: v_N_out_sel){
 				mark[w-1] -= 2;
 				
 				if(mark[w-1] !=0){
+					clock.tick("increment_k3");
 					k3[u-1] ++;
 					k3[v-1] ++;
 					k3[w-1] ++;
@@ -262,28 +296,44 @@ IntegerMatrix countOrtmann(IntegerMatrix edge_list){
 					// add {u,v} to T(w)
 					completing_triangle[w-1].insert(completing_triangle[w-1].end(), u);
 					completing_triangle[w-1].insert(completing_triangle[w-1].end(), v);
+					clock.tock("increment_k3");
 					
+					clock.tick("get_neighbour");
+					// {x e N+(w): x < u}
+					IntegerVector w_N_out_sel(neighbourhood[w-1][2].begin(), 
+											  lower_bound(neighbourhood[w-1][2].begin(), 
+											  neighbourhood[w-1][2].end(), u));
+					clock.tock("get_neighbour");
 					
-					IntegerVector w_N_out = as<List>(neighbourhood["out_neighbourhood"])[w-1];
-					IntegerVector w_N_out_sel = w_N_out[w_N_out < u];
-					
-					for(int x: w_N_out_sel){
+					clock.tick("increment_k4");
+					for(unsigned int x: w_N_out_sel){
+						clock.tick("increment_k4_if");
 						if(mark[x-1] == 3){
+							clock.tick("increment_k4_inc");
 							k4[u-1] ++;
 							k4[v-1] ++;
 							k4[w-1] ++;
 							k4[x-1] ++;
+							clock.tock("increment_k4_inc");
 						}
+						clock.tock("increment_k4_if");
 					}
+					clock.tock("increment_k4");
 				}
+				
 			}
+			clock.tock("w_for");
 		}
+		clock.tock("count_k3");
 		
-		for(int v: u_N_in){
-			IntegerVector v_N = as<List>(neighbourhood["total_neighbourhood"])[v-1];
-			IntegerVector v_N_sel = v_N[v_N < u];
+		clock.tick("count_c4");
+		for(unsigned int v: neighbourhood[u-1][1]){ //loop over N-(u)
+			// {w e N(v): w < u}
+			IntegerVector v_N_sel(neighbourhood[v-1][0].begin(), 
+								  lower_bound(neighbourhood[v-1][0].begin(), 
+								  neighbourhood[v-1][0].end(), u));
 			
-			for(int w: v_N_sel){
+			for(unsigned int w: v_N_sel){
 				processed[w-1] --;
 				
 				if(visited[w-1] > 0){
@@ -298,18 +348,19 @@ IntegerMatrix countOrtmann(IntegerMatrix edge_list){
 				}
 			}
 		}
+		clock.tock("count_c4");
 		
 		
 	}
-	
+	clock.tock("algorithm");
 	
 	// solve system of equations
 	IntegerMatrix all_non_induced_counts (n_nodes, 20);
 	IntegerMatrix all_induced_counts (n_nodes, 15);
 	
-	for(int t = 1; t <= n_nodes; t++){
+	for(unsigned int t = 1; t <= n_nodes; t++){
 		all_non_induced_counts(t-1, _) = non_induced_orbits(t, n_nodes, n_edges, 
-							  neighbourhood, deg, edge_list,
+							  neighbourhood, deg,
 							  k3, c4, k4, k3_edge, completing_triangle);
 							  
 		all_induced_counts(t-1, _) = compute_induced_orbits(all_non_induced_counts(t-1, _));
@@ -318,18 +369,21 @@ IntegerMatrix countOrtmann(IntegerMatrix edge_list){
 	colnames(all_induced_counts) = CharacterVector::create("o0", "o1", "o2", "o3", "o4", "o5",
 															   "o6", "o7", "o8", "o9", "o10", "o11",
 															   "o12", "o13", "o14");
+															   
+	clock.tock("total_count");
+	clock.stop("profiling_countOrtmann");
 
 	return all_induced_counts;
 }
 
 // [[Rcpp::export]]
 NumericMatrix GCM(IntegerMatrix induced_orbits){
-	int n_orbits = induced_orbits.ncol();
+	unsigned int n_orbits = induced_orbits.ncol();
 	
 	NumericMatrix GCM = NumericMatrix::diag(n_orbits, 1.0);
 	
-	for(int i = 0; i < n_orbits - 1; i++){
-		for(int j = i + 1; j < n_orbits; j++){
+	for(unsigned int i = 0; i < n_orbits - 1; i++){
+		for(unsigned int j = i + 1; j < n_orbits; j++){
 			GCM(i, j) =  spearman(induced_orbits(_, i), induced_orbits(_, j));
 			GCM(j, i) =  spearman(induced_orbits(_, i), induced_orbits(_, j));
 		}
@@ -340,12 +394,12 @@ NumericMatrix GCM(IntegerMatrix induced_orbits){
 
 // [[Rcpp::export]]
 double GCD(NumericMatrix X, NumericMatrix Y){
-	int n_orbits = X.ncol();
+	unsigned int n_orbits = X.ncol();
 	
 	double GCD;
 	
-	for(int i = 0; i < n_orbits - 1; i++){
-		for(int j = i + 1; j < n_orbits; j++){
+	for(unsigned int i = 0; i < n_orbits - 1; i++){
+		for(unsigned int j = i + 1; j < n_orbits; j++){
 			GCD += pow((X(i, j) - Y(i, j)), 2);
 		}
 	}
