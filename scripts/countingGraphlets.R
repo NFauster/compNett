624926feec6c0c1613acc6c0f374e105c331e5db
Norman FRM Fauster <norman.fauster@outlook.com>
2022-09-01 19:01:34 +0200

    modified such that the file contains only the function for the algorithm

--- scripts/countingGraphlets.R
+++ scripts/countingGraphlets.R
@@ -8,139 +8,115 @@ library(dplyr)
 source(here::here("scripts/functions.R"))
 
 
-# Example data ----
-## Ex 1 ====
-edge_list <- matrix(c(1,3,
-                      1,2,
-                      2,3,
-                      2,4),
-                    ncol = 2,
-                    byrow = T)
-
-
-## Ex 2 ====
-edge_list <- matrix(c(1,3,
-                      5,1,
-                      2,3,
-                      2,5,
-                      3,4,
-                      5,3,
-                      4,5),
-                    ncol = 2,
-                    byrow = T)
-
-## Ex 3 ====
-edge_list <- igraph::as_edgelist(igraph::erdos.renyi.game(1e3, p.or.m = .2))
-
-
-# Graph properties ----
-n_nodes <- max(edge_list)     # number of nodes
-n_edges <- nrow(edge_list)    # number of edges
-
-
-# Initialise variables ----
-mark <- vector(mode = "numeric", length = n_nodes)
-visited <- vector(mode = "numeric", length = n_nodes)
-processed <- vector(mode = "numeric", length = n_nodes)
-k3 <- vector(mode = "numeric", length = n_nodes)
-k4 <- vector(mode = "numeric", length = n_nodes)
-c4 <- vector(mode = "numeric", length = n_nodes)
-
-
-# Order nodes ----
-## according to node degree
-order_nodes <- degree(edge_list, directed = FALSE) %>%
-  sort() %>%
-  names() %>%
-  {
-    data.frame(original = as.numeric((.)),
-               ordered = 1:length((.)))
-  }
-
-
-# Orient graph ----
-edge_list_rename <- edge_list %>%
-  as.data.frame() %>%
-  left_join(order_nodes,
-            by = c("V1" = "original"))%>%
-  left_join(order_nodes,
-            by = c("V2" = "original"),
-            suffix = c(".from", ".to")) %>%
-  select(-c("V1", "V2")) %>%
-  as.matrix()
-
-
-#
-edge_list_rename_redirect <- apply(edge_list_rename, 
-      MARGIN = 1, 
-      FUN = redirect_edge,
-      simplify = TRUE) %>%
-  t()
-
-# Compute neighbourhoods ----
-neighbourhood <- list_neighbourhood(edge_list_rename_redirect,
-                                    directed = TRUE)
-
-
-# Algorithm ----
-tictoc::tic()
-for(u in 2:n_nodes){
-  u_N_in <- neighbourhood$in_neighbourhood[[u]]         # N-(u)
+# Prepare the network ----
+prep_network <- function(edge_list){
+  ## Order nodes ====
+  ## according to node degree
+  order_nodes <- degree(edge_list, directed = FALSE) %>%
+    sort() %>%
+    names() %>%
+    {
+      data.frame(original = as.numeric((.)),
+                 ordered = 1:length((.)))
+    }
   
-  mark[u_N_in] <- mark[u_N_in]+1
+  ## Orient graph ====
+  edge_list_rename <- edge_list %>%
+    as.data.frame() %>%
+    left_join(order_nodes,
+              by = c("V1" = "original"))%>%
+    left_join(order_nodes,
+              by = c("V2" = "original"),
+              suffix = c(".from", ".to")) %>%
+    select(-c("V1", "V2")) %>%
+    as.matrix()
   
-  for(v in u_N_in){
-    mark[v] <- mark[v] - 1
+  
+  edge_list_rename_redirect <- apply(edge_list_rename, 
+                                     MARGIN = 1, 
+                                     FUN = redirect_edge,
+                                     simplify = TRUE) %>%
+    t()
+  
+  return(edge_list_rename_redirect)
+}
+
+
+# Counting algorithm ----
+countOrtmannR <- function(edge_list_rename_redirect){
+  ## Compute neighbourhoods ====
+  neighbourhood <- list_neighbourhood(edge_list_rename_redirect,
+                                      directed = TRUE)
+  
+  
+  n_nodes <- max(edge_list_rename_redirect)     # number of nodes
+  n_edges <- nrow(edge_list_rename_redirect)    # number of edges
+  
+  
+  ## Initialise variables ====
+  mark <- vector(mode = "numeric", length = n_nodes)
+  visited <- vector(mode = "numeric", length = n_nodes)
+  processed <- vector(mode = "numeric", length = n_nodes)
+  k3 <- vector(mode = "numeric", length = n_nodes)
+  k4 <- vector(mode = "numeric", length = n_nodes)
+  c4 <- vector(mode = "numeric", length = n_nodes)
+  
+  
+  ## Algorithm ====
+  for(u in 2:n_nodes){
+    u_N_in <- neighbourhood$in_neighbourhood[[u]]         # N-(u)
     
+    mark[u_N_in] <- mark[u_N_in]+1
     
-    v_N <- neighbourhood$total_neighbourhood[[v]]     # N(v)
-    v_N_sel <- v_N[v_N < u]                           # {w e N(v): w<u}
-    
-    visited[v_N_sel] <- visited[v_N_sel] + 1
-    processed[v_N_sel] <- processed[v_N_sel] + 1
-    
-    #####
-    v_N_out <- neighbourhood$out_neighbourhood[[v]]   # N+(v)
-    v_N_out_sel <- v_N_out[v_N_out < u]               # {w e N+(v): w<u}
-    
-    mark[v_N_out_sel] <- mark[v_N_out_sel] + 2
-    for(w in v_N_out_sel){
-      mark[w] <- mark[w] - 2
+    for(v in u_N_in){
+      mark[v] <- mark[v] - 1
       
-      if(mark[w] != 0){
-        k3[c(u,v,w)] <- k3[c(u,v,w)] + 1
+      
+      v_N <- neighbourhood$total_neighbourhood[[v]]     # N(v)
+      v_N_sel <- v_N[v_N < u]                           # {w e N(v): w<u}
+      
+      visited[v_N_sel] <- visited[v_N_sel] + 1
+      processed[v_N_sel] <- processed[v_N_sel] + 1
+      
+      ####
+      v_N_out <- neighbourhood$out_neighbourhood[[v]]   # N+(v)
+      v_N_out_sel <- v_N_out[v_N_out < u]               # {w e N+(v): w<u}
+      
+      mark[v_N_out_sel] <- mark[v_N_out_sel] + 2
+      for(w in v_N_out_sel){
+        mark[w] <- mark[w] - 2
         
-        ###
-        w_N_out <- neighbourhood$out_neighbourhood[[w]]   # N+(w)
-        w_N_out_sel <- w_N_out[w_N_out < u]               # {x e N+(w): x<u}
+        if(mark[w] != 0){
+          k3[c(u,v,w)] <- k3[c(u,v,w)] + 1
+          
+          ###
+          w_N_out <- neighbourhood$out_neighbourhood[[w]]   # N+(w)
+          w_N_out_sel <- w_N_out[w_N_out < u]               # {x e N+(w): x<u}
+          
+          for(x in w_N_out_sel){
+            if(mark[x] == 3) k4[c(x,w,v,u)] <- k4[c(x,w,v,u)] + 1
+          }
+        }
+      }
+    }
+    
+    
+    for(v in u_N_in){
+      v_N <- neighbourhood$total_neighbourhood[[v]]     # N(v)
+      v_N_sel <- v_N[v_N < u]                           # {w e N(v): w<u}
+      
+      for(w in v_N_sel){
+        processed[w] <- processed[w] - 1
+        if(visited[w] > 0) c4[v] <- c4[v] + visited[w] - 1
         
-        for(x in w_N_out_sel){
-          if(mark[x] == 3) k4[c(x,w,v,u)] <- k4[c(x,w,v,u)] + 1
+        if(processed[w] == 0){
+          c4[c(u,w)] <- c4[c(u,w)] + choose(visited[w], 2)
+          visited[w] <- 0
         }
       }
     }
   }
-  
-  
-  for(v in u_N_in){
-    v_N <- neighbourhood$total_neighbourhood[[v]]     # N(v)
-    v_N_sel <- v_N[v_N < u]                           # {w e N(v): w<u}
-    
-    for(w in v_N_sel){
-      processed[w] <- processed[w] - 1
-      if(visited[w] > 0) c4[v] <- c4[v] + visited[w] - 1
-      
-      if(processed[w] == 0){
-        c4[c(u,w)] <- c4[c(u,w)] + choose(visited[w], 2)
-        visited[w] <- 0
-      }
-    }
-  }
+  # tictoc::toc()
 }
-tictoc::toc()
 
 
-# Check with orca ----
-tictoc::tic()
-orca::count4(matrix(as.integer(edge_list),ncol=2,byrow=F))
-tictoc::toc()
